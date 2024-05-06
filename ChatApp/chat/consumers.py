e67407977b3d8ac333a85767bf363fd127cf839d
Nadjib ATOUI <nadjib.atoui@WATSON.LOC>
2024-05-06 05:58:18 +0100

    some changes

--- ChatApp/chat/consumers.py
+++ ChatApp/chat/consumers.py
@@ -1,31 +1,36 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from confluent_kafka import Producer
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.producer = Producer({'bootstrap.servers': 'localhost:9092'})
+
     async def connect(self):
         self.roomGroupName = "group_chat_gfg"
         await self.channel_layer.group_add(
-            self.roomGroupName ,
+            self.roomGroupName,
             self.channel_name
         )
         await self.accept()
-    async def disconnect(self , close_code):
+
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.roomGroupName , 
-            self.channel_layer 
+            self.roomGroupName,
+            self.channel_layer
         )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
-        await self.channel_layer.group_send(
-            self.roomGroupName,{
-                "type" : "sendMessage" ,
-                "message" : message , 
-                "username" : username ,
-            })
-    async def sendMessage(self , event) : 
+
+        # Produce message to Kafka topic
+        self.producer.produce(self.roomGroupName, key=username, value=message)
+        self.producer.flush()
+
+    async def sendMessage(self, event):
         message = event["message"]
         username = event["username"]
-        await self.send(text_data = json.dumps({"message":message ,"username":username}))
-      
+        await self.send(text_data=json.dumps({"message": message, "username": username}))
