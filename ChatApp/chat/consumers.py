339565223e182fc174a870357e29f53e3bf473bf
Nadjib ATOUI <nadjib.atoui@WATSON.LOC>
2024-05-07 05:28:18 +0100

    make the chat only between specifique users

--- ChatApp/chat/consumers.py
+++ ChatApp/chat/consumers.py
@@ -8,27 +8,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
     async def connect(self):
-        self.roomGroupName = "group_chat_gfg"
+        self.sender_username = self.scope['url_route']['kwargs']['sender_username']
+        self.receiver_username = self.scope['url_route']['kwargs']['receiver_username']
+        self.room_name = f"chat_{self.sender_username}_{self.receiver_username}"
         await self.channel_layer.group_add(
-            self.roomGroupName,
+            self.room_name,
             self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.roomGroupName,
-            self.channel_layer
+            self.room_name,
+            self.channel_name
         )
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        username = text_data_json["username"]
-
-        # Produce message to Kafka topic
-        self.producer.produce(self.roomGroupName, key=username, value=message)
-        self.producer.flush()
+        sender_username = text_data_json["username"]
+        
+        # Ensure only the allowed users can send messages
+        if sender_username == self.sender_username:
+            # Produce message to Kafka topic
+            self.producer.produce(self.room_name, key=sender_username, value=message)
+            self.producer.flush()
 
     async def sendMessage(self, event):
         message = event["message"]
